1595db879034ab8ede4ee93c1caae36488c07ed3
applikasiku <amelnewsid@gmail.com>
2026-09-17 12:50:03 +0700

    v2.2: load clean watch styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './styles.css'
 import './extras.css'
 import './tiktok-v2.css'
 import './v21.css'
+import './v22.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
