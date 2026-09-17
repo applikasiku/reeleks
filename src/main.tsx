eda295afa3778d4df40d36c696351b627b595ad4
applikasiku <amelnewsid@gmail.com>
2026-09-17 13:09:45 +0700

    v2.4: load transition and comments styles

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './extras.css'
 import './tiktok-v2.css'
 import './v21.css'
 import './v22.css'
+import './v24.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
